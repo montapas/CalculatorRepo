3b6f4320d209efd37dbef9eafb9c019ebf5cdca4
montapas <montapas@gmail.com>
2018-12-24 10:41:59 +0530

    Update Calculator.java

--- src/main/java/com/cts/examples/cal/Calculator.java
+++ src/main/java/com/cts/examples/cal/Calculator.java
@@ -3,7 +3,7 @@ package com.cts.examples.cal;
 public class Calculator implements ICalculator {
 
 	public int sum(int a, int b) {
-		 int i;
+		 
 		return a + b;  
 		
 	}
@@ -17,7 +17,7 @@ public class Calculator implements ICalculator {
 	}
 
 	public int divison(int a, int b)  {
-		String.format("%< is equals to %d", 2);   //Noncompliant; the argument index '<' refers to the previous format specifier but there isn't one
+		 
 		return a / b;
 	}
 	 
